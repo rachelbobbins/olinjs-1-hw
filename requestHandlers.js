a72bb4e6a92d0c73a98f6098e4d9d8af04ef7b07
Rachel <rachelheidi@gmail.com>
2013-01-22 21:51:05 -0500

    Added rudimentary request handler

---
+++ requestHandlers.js
@@ -0,0 +1,10 @@
+function start() {
+  console.log("Request handler 'start' was called.");
+}
+
+function upload() {
+  console.log("Request handler 'upload' was called.");
+
+  xports.start = start;
+  exports.upload = upload;
+}
